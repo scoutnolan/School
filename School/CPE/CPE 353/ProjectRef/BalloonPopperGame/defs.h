f6362b0b0d8a08d9e83eb4d1864353c31825149d
Nolan Anderson <nolan@nolan-2.local>
2021-08-24 13:41:50 -0500

    Final push for CPE courses.

--- School/CPE/CPE 353/ProjectRef/BalloonPopperGame/defs.h
+++
@@ -1,7 +0,0 @@
-#ifndef DEFS_H
-#define DEFS_H
-
-const int XMAX = 640;       // were qreals in the lab
-const int YMAX = 480;
-
-#endif // DEFS_H
--- School/CPE/CPE 381/Homeworks/Homework 4/H4_2.m
+++
@@ -1,8 +0,0 @@
-%% Nolan Anderson
-% CPE 381 Homework 4 #2
-
-Fs = 20;      
-Ts = 1/Fs;     
-t = -6:Ts:6;     % Time 
-h = sin(pi*t)/(pi*t);
-plot(t, h);
